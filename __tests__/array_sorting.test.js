cddd44b81d55e27b5c18c04330256c32bb1c3f48
Usarneme <jamestcoop@gmail.com>
2021-06-25 09:25:42 -0700

    :art: adds empty test files for the other four whiteboard prompts

---
+++ __tests__/array_sorting.test.js
@@ -0,0 +1,10 @@
+const {
+  stringToUrlReplace,
+} = require('../src/string_to_url')
+
+describe("Testing the String To URL Prompt", () => {
+  test("sanity check", () => {
+    expect(true).toBeTruthy()
+  })
+
+})
