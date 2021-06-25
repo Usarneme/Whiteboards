2e9821560784edfe5c15d913a3f8bdaf61c54234
Usarneme <jamestcoop@gmail.com>
2021-06-25 14:31:43 -0700

    :tada: adds passing test and code for selection sort from array_sorting prompt

--- __tests__/array_sorting.test.js
+++ __tests__/array_sorting.test.js
@@ -1,10 +1,15 @@
 const {
-  stringToUrlReplace,
-} = require('../src/string_to_url')
+  selectionSort,
+} = require('../src/array_sorting')
 
 describe("Testing the String To URL Prompt", () => {
   test("sanity check", () => {
     expect(true).toBeTruthy()
   })
 
+  test("selection sort should return a sorted array", () => {
+    const inputArray = [9,5,3,2,6,78,2,12,1]
+    const outputArray = [1,2,2,3,5,6,9,12,78]
+    expect(selectionSort(inputArray)).toEqual(outputArray)
+  })
 })
---
+++ src/array_sorting.js
@@ -0,0 +1,28 @@
+// Selection Sort
+// Repeatedly find the minimum element in the unsorted part of an array and put it at the beginning of sorted part of the array
+
+const selectionSort = inputArray => {
+  if (inputArray.length < 2) return inputArray
+  let unsortedStartingIndex = 0
+  while (unsortedStartingIndex < inputArray.length) {
+    const smallestIndex = findSmallestElement(inputArray.slice(unsortedStartingIndex)) + unsortedStartingIndex
+    const smallestElement = inputArray[smallestIndex]
+    // remove the smallest element now that it has been copied
+    inputArray.splice(smallestIndex, 1)
+    // insert the next smallest element in place
+    inputArray.splice(unsortedStartingIndex, 0, smallestElement)
+    // update the starting index of the unsorted part of the array
+    unsortedStartingIndex += 1
+  }
+  return inputArray
+}
+
+const findSmallestElement = inputArray => {
+  let smallestIndex = 0
+  inputArray.forEach((element, index) => {
+    if (element < inputArray[smallestIndex]) smallestIndex = index
+  })
+  return smallestIndex
+}
+
+module.exports = { selectionSort }
