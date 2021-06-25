94139eb894ef96df69908dac95a4393106d568bc
Usarneme <jamestcoop@gmail.com>
2021-06-25 09:14:12 -0700

    :tada: adds passing tests for the four implementations of string to url

--- __tests__/string_to_url.test.js
+++ __tests__/string_to_url.test.js
@@ -9,4 +9,23 @@ describe("Testing the String To URL Prompt", () => {
   test("sanity check", () => {
     expect(true).toBeTruthy()
   })
+
+  const inputString = "This is a test input"
+  const outputString = "This%20is%20a%20test%20input"
+
+  test("string to url using replace returns proper output", () => {
+    expect(stringToUrlReplace(inputString)).toEqual(outputString)
+  })
+
+  test("string to url using split+join returns proper output", () => {
+    expect(stringToUrlSplitJoin(inputString)).toEqual(outputString)
+  })
+
+  test("string to url using loop returns proper output", () => {
+    expect(stringToUrlLoop(inputString)).toEqual(outputString)
+  })
+
+  test("string to url using recursion returns proper output", () => {
+    expect(stringToUrlRecursive(inputString)).toEqual(outputString)
+  })
 })
